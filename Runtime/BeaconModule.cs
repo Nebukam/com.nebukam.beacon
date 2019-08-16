369d0bbb968f951dacc2c7eb8676d64ad49687fb
Tim <nebukam@gmail.com>
2019-08-16 02:47:17 -0400

    Update BeaconModule.cs

--- Runtime/BeaconModule.cs
+++ Runtime/BeaconModule.cs
@@ -1,12 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Nebukam.JobAssist;
 
-namespace Packages.com.nebukam.beacon.Runtime
+namespace Nebukam.Beacon
 {
-    class BeaconModule
+    public abstract class BeaconModule<T> : SingletonBehaviour<T>
+        where T : Component, new()
     {
+
+        public delegate void OnProcessorComplete(IProcessor processor);
+
+        protected List<IProcessor> m_processes = new List<IProcessor>();
+        protected Dictionary<IProcessor, OnProcessorComplete> m_processesCallbacks = new Dictionary<IProcessor, OnProcessorComplete>();
+
+        protected override void Init()
+        {
+            
+        }
+
+        protected override void Tick(float delta)
+        {
+            for (int i = 0, count = m_processes.Count; i < count; i++)
+                m_processes[i].Schedule(delta);
+        }
+
+        protected override void LateTick(float delta)
+        {
+            IProcessor p;
+            OnProcessorComplete callback;
+            for (int i = 0, count = m_processes.Count; i < count; i++)
+            {
+                p = m_processes[i];
+                if (p.TryComplete() && m_processesCallbacks.TryGetValue(p, out callback))
+                    callback(p);
+            }
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+
+            base.Dispose(disposing);
+            if (!disposing) { return; }
+
+            IProcessor p;
+            IProcessorGroup pGroup;
+            for(int i = 0, count = m_processes.Count; i < count; i++)
+            {
+                p = m_processes[i];
+                pGroup = p as IProcessorGroup;
+                if (pGroup != null)
+                    pGroup.DisposeAll();
+                else
+                    p.Dispose();
+            }
+
+            m_processes.Clear();
+            m_processes = null;
+
+            m_processesCallbacks.Clear();
+
+        }
+
+        protected override void FixedTick()
+        {
+            
+        }
+
     }
 }
